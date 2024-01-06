351825b13778d3c684450ef287d88e5f3183493f
Sheryll Dumapal <sheryllhhs@outlook.com>
2024-01-07 00:39:25 +0100

    added routes and login/signup/notfound/home/chat pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import Header from "./components/Header";
+import {Routes, Route} 'react-router-dom'; 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <header />
+      <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/chat" element={<Chat />} />
+    <Route path="/notFound" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
